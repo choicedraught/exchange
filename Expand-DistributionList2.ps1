ba7c66e8e3843b7bdf4ac4b5c5b5b9a3e7b41c31
choicedraught <jack.bryant@gmail.com>
2017-07-08 17:45:01 +0800

    Create Expand-DistributionList2.ps1
    First commit

---
+++ Expand-DistributionList2.ps1
@@ -0,0 +1,37 @@
+ 
+Import-Module ActiveDirectory 
+
+ Param (
+    [Parameter(Mandatory=$true)][string]$Group
+) 
+
+$newbatch = @(); 
+$members = Get-ADGroupMember -Identity $Group -Recursive
+
+foreach ($member in $members) { 
+    if ((get-recipient $member.name).recipienttype -eq "UserMailbox") { 
+        $mbox = get-mailbox $member.SamAccountName; 
+        
+        $row = New-Object PSObject;  
+        $row | Add-Member -MemberType NoteProperty -Name "EmailAddress" -Value $mbox.PrimarySMTPAddress; 
+        $row | Add-Member -MemberType NoteProperty -Name "BadItemCount" -Value "0"; 
+        $row | Add-Member -MemberType NoteProperty -Name "StaffNumber" -Value $mbox.SamAccountName; 
+        $row | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $mbox.DisplayName; 
+        $row | Add-Member -MemberType NoteProperty -Name "TotalItemSizeinGB" -Value "0"; 
+        $row | Add-Member -MemberType NoteProperty -Name "TotalItemSizeinMB" -Value "0"; 
+        $newbatch += $row 
+    } else {
+        $mbox = get-RemoteMailbox $member.SamAccountName; 
+        $row = New-Object PSObject;  
+        $row | Add-Member -MemberType NoteProperty -Name "EmailAddress" -Value "MIGRATED: $($mbox.PrimarySMTPAddress)"; 
+        $row | Add-Member -MemberType NoteProperty -Name "BadItemCount" -Value "0"; 
+        $row | Add-Member -MemberType NoteProperty -Name "StaffNumber" -Value $mbox.SamAccountName; 
+        $row | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $mbox.DisplayName; 
+        $row | Add-Member -MemberType NoteProperty -Name "TotalItemSizeinGB" -Value "0"; 
+        $row | Add-Member -MemberType NoteProperty -Name "TotalItemSizeinMB" -Value "0"; 
+        $newbatch += $row 
+    }
+} 
+
+Write-Host "Total recursive uniquie members: $($newbatch.length)" 
+$newbatch | Export-csv -path ".\$($Group).csv" -notypeinformation
